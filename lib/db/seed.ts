12f29be440e0686dac9dc19666f7ada6d436120c
Andrew Hsu <100561529+andrewhsugithub@users.noreply.github.com>
2024-10-21 02:04:19 +0800

    feat: add db and public key folder (#2)
    * feat: add db and public key folder
    
    * fix: disable restart for db
    
    * feat: add docker for connecting to remote server

---
+++ lib/db/seed.ts
@@ -0,0 +1,12 @@
+﻿import { db } from "./db.js";
+import { faker } from "@faker-js/faker";
+import { avatars, type AvatarType } from "./schema/avatar.js";
+
+// const insertUser = async (user: UserType) => {
+//   return db.insert(users).values(user);
+// };
+
+// const randomName = faker.person.fullName();
+// console.log(randomName);
+// const newUser: UserType = { name: randomName };
+// // await insertUser(newUser);
